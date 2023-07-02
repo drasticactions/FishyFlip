080855fdade42a2d399f2b64ddda2499b60c0662
Tim Miller <innerlogic4321@gmail.com>
2023-07-02 20:45:43 +0900

    Initial Commit

---
+++ src/FishyFlip.DebugApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System.Net.NetworkInformation;
+using System.Text.Json;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+var serviceCollection = new ServiceCollection();
+
+Ioc.Default.ConfigureServices(serviceCollection
+       .BuildServiceProvider());
+
+Console.WriteLine("Press any key to exit...");
