6ee42cfee007c55518a44c20013e6a1a982670a7
Tim Miller <innerlogic4321@gmail.com>
2023-07-14 18:34:15 +0900

    Initial

--- apps/bskyimg/Program.cs
+++ apps/bskyimg/Program.cs
@@ -3,17 +3,22 @@
 // </copyright>
 
 using CommandLine;
+using FishyFlip;
+using FishyFlip.Models;
+using FishyFlip.Tools;
+using Microsoft.Extensions.Logging.Debug;
+using Error = CommandLine.Error;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(RunOptions)
+            .WithParsed(async (options) => await RunOptions(options))
             .WithNotParsed(HandleParseError);
     }
 
-    static void RunOptions(Options options)
+    static async Task RunOptions(Options options)
     {
         var username = options.Username ?? System.Environment.GetEnvironmentVariable("BLUESKY_USERNAME");
         var password = options.Password ?? System.Environment.GetEnvironmentVariable("BLUESKY_PASSWORD");
@@ -26,10 +31,86 @@ internal class Program
 
         var host = options.Host ?? "bsky.social";
 
+        if (!Uri.TryCreate(host, UriKind.Absolute, out Uri? uri))
+        {
+            uri = new Uri($"https://{host}");
+        }
+        
+        var debugLog = new DebugLoggerProvider();
+        var atProtocolBuilder = new ATProtocolBuilder()
+            .EnableAutoRenewSession(true)
+            .WithUrl(uri)
+            .WithUserAgent("bskyimg")
+            .WithLogger(debugLog.CreateLogger("bskyimg"));
+        
+        var atProtocol = atProtocolBuilder.Build();
+
+        var login = await atProtocol.Server.CreateSessionAsync(username, password);
+        if (login.IsT1)
+        {
+            Console.WriteLine($"{login.AsT1.StatusCode}: {login.AsT1.Detail}");
+            return;
+        }
+        
+        var imageList = new List<Image>();
         foreach (string file in options.Images)
         {
-            Console.WriteLine($"Processing file: {file}");
+            StreamContent content;
+            try
+            {
+                content = StreamContentHelpers.FromFilePath(file);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            var image = await atProtocol.Repo.UploadBlobAsync(content);
+            image.Switch(
+                success =>
+                {
+                    imageList.Add(success.Blob.ToImage());
+                },
+                error =>
+                {
+                    Console.WriteLine($"{error.StatusCode}, {error.Detail}");
+                }
+            );
+
+            if (image.IsT1)
+            {
+                // Failed, break.
+                return;
+            }
         }
+
+        if (!imageList.Any())
+        {
+            Console.WriteLine("No Images Uploaded.");
+            return;
+        }
+
+        var imageEmbedList = new List<ImageEmbed>();
+        var altImages = options.AltText?.ToArray() ?? Array.Empty<string>();
+        for (var i = 0; i < imageList.Count; i++)
+        {
+            var img = imageList[i];
+            var altText = altImages[i] ?? string.Empty;
+            imageEmbedList.Add(new ImageEmbed(img, altText));
+        }
+
+        var post = await atProtocol.Repo.CreatePostAsync(options.Text ?? string.Empty, null, new ImagesEmbed(imageEmbedList.ToArray()));
+        await post.SwitchAsync(
+            async postSuccess =>
+            {
+                Console.WriteLine($"Post Created: {postSuccess.Uri}");
+            },
+            async postError =>
+            {
+                Console.WriteLine($"{postError.StatusCode}, {postError.Detail}");
+            }
+        );
     }
 
     static void HandleParseError(IEnumerable<Error> errs)
