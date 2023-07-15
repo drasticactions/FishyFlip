c7b83dcb73022910882563a73ae5b5c6b26accff
Tim Miller <innerlogic4321@gmail.com>
2023-07-15 15:16:12 +0900

    Nuget

---
+++ apps/bskycli/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// Program Root.
+/// </summary>
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+    }
+}
