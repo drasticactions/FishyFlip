a2a269c40adc22753b5a2dc3a5597304878552f9
Tim Miller <innerlogic4321@gmail.com>
2023-07-17 22:56:50 +0900

    API

---
+++ src/FishyFlip/Tools/ATNetworkErrorException.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ATNetworkErrorException.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace FishyFlip.Tools;
+
+/// <summary>
+/// AT Network Error Exception.
+/// Thrown if given a failed result from ATProtocol.
+/// </summary>
+public class ATNetworkErrorException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ATNetworkErrorException"/> class.
+    /// </summary>
+    /// <param name="error">Error message.</param>
+    public ATNetworkErrorException(Error error)
+        : base($"{error.StatusCode}: {error.Detail}")
+    {
+        this.Error = error;
+    }
+
+    /// <summary>
+    /// Gets the base error.
+    /// </summary>
+    public Error Error { get; }
+}
---
+++ src/FishyFlip/Tools/ResultExtensions.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ResultExtensions.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace FishyFlip.Tools;
+
+/// <summary>
+/// Result Extensions.
+/// </summary>
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Throw ATNetworkErrorException if Result is an Error object..
+    /// </summary>
+    /// <typeparam name="T">Base object.</typeparam>
+    /// <param name="result">Result object.</param>
+    /// <returns>Result object if success.</returns>
+    /// <exception cref="ATNetworkErrorException">Thrown if the result is an error object.</exception>
+    public static T HandleResult<T>(this Result<T> result)
+    {
+        if (result.IsT1)
+        {
+            throw new ATNetworkErrorException(result.AsT1);
+        }
+
+        return result.AsT0;
+    }
+}
