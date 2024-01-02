fac37fcdd6f99414b974502e187c8b8577d2834c
Tim Miller <innerlogic4321@gmail.com>
2024-01-02 17:23:57 +0900

    Start User Profile page

--- website/FishyFlipSite/Program.cs
+++ website/FishyFlipSite/Program.cs
@@ -10,6 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLogging();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
