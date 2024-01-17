7f66d299098d22e72a51c52dadcda7dca73dda45
Tim Miller <innerlogic4321@gmail.com>
2024-01-17 21:25:43 +0900

    Update comments

--- src/FishyFlip/Models/Label.cs
+++ src/FishyFlip/Models/Label.cs
@@ -4,4 +4,13 @@
 
 namespace FishyFlip.Models;
 
+/// <summary>
+/// Metadata tag on an atproto resource (eg, repo or record).
+/// </summary>
+/// <param name="Src">DID of the actor who created this label.</param>
+/// <param name="Uri">AT URI of the record, repository (account), or other resource that this label applies to.</param>
+/// <param name="Cid">Optionally, CID specifying the specific version of 'uri' resource this label applies to.</param>
+/// <param name="Val">The short string name of the value or type of this label.</param>
+/// <param name="Neg">If true, this is a negation label, overwriting a previous label.</param>
+/// <param name="Cts">Timestamp when this label was created.</param>
 public record Label(ATUri Src, string Uri, Cid Cid, string Val, bool Neg, DateTime Cts);
