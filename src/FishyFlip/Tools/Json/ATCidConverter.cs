d74b49b291e5a192f652da00d67fad839450a159
Tim Miller <innerlogic4321@gmail.com>
2024-02-23 03:25:04 +0900

    Actually fix converter by using the right type

--- src/FishyFlip/Tools/Json/ATCidConverter.cs
+++ src/FishyFlip/Tools/Json/ATCidConverter.cs
@@ -7,10 +7,10 @@ namespace FishyFlip.Tools.Json;
 /// <summary>
 /// Converts a <see cref="ATCid"/> object to and from JSON.
 /// </summary>
-public class ATCidConverter : JsonConverter<ATCid?>
+public class ATCidConverter : JsonConverter<Cid?>
 {
     /// <inheritdoc/>
-    public override ATCid? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override Cid? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (JsonDocument.TryParseValue(ref reader, out var doc))
         {
@@ -19,14 +19,14 @@ public class ATCidConverter : JsonConverter<ATCid?>
                 if (doc.RootElement.TryGetProperty("$link", out var type))
                 {
                     var typeString = type.GetString()?.Trim() ?? string.Empty;
-                    return string.IsNullOrEmpty(typeString) ? null : Cid.Decode(typeString).ToString();
+                    return string.IsNullOrEmpty(typeString) ? null : Cid.Decode(typeString);
                 }
             }
 
             if (doc.RootElement.ValueKind is JsonValueKind.String)
             {
                 var typeString = doc.RootElement.GetString()?.Trim() ?? string.Empty;
-                return string.IsNullOrEmpty(typeString) ? null : typeString;
+                return string.IsNullOrEmpty(typeString) ? null : Cid.Decode(typeString);
             }
         }
 
@@ -47,7 +47,7 @@ public class ATCidConverter : JsonConverter<ATCid?>
     }
 
     /// <inheritdoc/>
-    public override void Write(Utf8JsonWriter writer, ATCid? value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, Cid? value, JsonSerializerOptions options)
     {
         writer.WriteStringValue(value?.ToString());
     }
