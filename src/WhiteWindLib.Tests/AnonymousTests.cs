d4df65f8837d29db917a0af02088c3a0728c61ca
Tim Miller <innerlogic4321@gmail.com>
2024-06-16 21:25:01 +0900

    Update tests

--- src/WhiteWindLib.Tests/AnonymousTests.cs
+++ src/WhiteWindLib.Tests/AnonymousTests.cs
@@ -13,17 +13,24 @@ namespace WhiteWindLib.Tests;
 [TestClass]
 public class AnonymousTests
 {
+    static string did;
+
+    static string aturi;
+
     static ATProtocol proto;
     static WhiteWindBlog blog;
 
     [ClassInitialize]
     public static void ClassInitialize(TestContext context)
     {
+        did = (string?)context.Properties["BLUESKY_TEST_DID"] ?? throw new ArgumentNullException();
+        aturi = (string?)context.Properties["BLUESKY_TEST_ATURI"] ?? throw new ArgumentNullException();
+        string instance = (string?)context.Properties["BLUESKY_INSTANCE_URL"] ?? throw new ArgumentNullException();
         var debugLog = new DebugLoggerProvider();
         var atProtocolBuilder = new ATProtocolBuilder()
             .EnableAutoRenewSession(false)
-            .WithInstanceUrl(new Uri("https://bsky.social"))
-            .WithLogger(debugLog.CreateLogger("FishyFlipTests"));
+            .WithInstanceUrl(new Uri(instance))
+            .WithLogger(debugLog.CreateLogger("WhiteWindLibTests"));
         AnonymousTests.proto = atProtocolBuilder.Build();
         blog = new WhiteWindBlog(proto);
     }
@@ -31,8 +38,8 @@ public class AnonymousTests
     [TestMethod]
     public async Task GetAuthorPostsTest()
     {
-        var did = ATDid.Create("did:plc:fzkpgpjj7nki7r5rhtmgzrez");
-        var (result, error) = await blog.GetAuthorPostsAsync(did!);
+        var did = ATDid.Create(AnonymousTests.did);
+        var (result, error) = await blog.GetAuthorEntriesAsync(did!);
         Assert.IsNull(error);
         Assert.IsNotNull(result);
         Assert.IsTrue(result!.Records.Length > 0);
@@ -41,7 +48,7 @@ public class AnonymousTests
     [TestMethod]
     public async Task GetAuthorPostTest()
     {
-        var postUri = ATUri.Create("at://did:plc:fzkpgpjj7nki7r5rhtmgzrez/com.whtwnd.blog.entry/3kutsnrkgvk2o");
+        var postUri = ATUri.Create(AnonymousTests.aturi);
         var (result, error) = await blog.GetEntryAsync(postUri.Did!, postUri.Rkey);
         Assert.IsNull(error);
         Assert.IsNotNull(result);
