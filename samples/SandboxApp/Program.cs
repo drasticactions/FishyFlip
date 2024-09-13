1ff5305f7b56ce55341e37b1ff87ad414f91cd76
Timothy Miller <innerlogic4321@gmail.com>
2024-09-13 14:54:30 +0900

    Cleanup, add Sandbox app

---
+++ samples/SandboxApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+/*
+ * This is a sandbox app for testing FishyFlip functions.
+ */
+
+using FishyFlip;
+using Microsoft.Extensions.Logging.Debug;
+
+Console.WriteLine("Hello, Sandbox!");
+
+var debugLog = new DebugLoggerProvider();
+
+// You can set a custom url with WithInstanceUrl
+var atWebProtocolBuilder = new ATWebSocketProtocolBuilder()
+ .WithLogger(debugLog.CreateLogger("FishyFlipDebug"));
+var atWebProtocol = atWebProtocolBuilder.Build();
+
+var atProtocolBuilder = new ATProtocolBuilder()
+ .WithLogger(debugLog.CreateLogger("FishyFlipDebug"));
+var atProtocol = atProtocolBuilder.Build();
