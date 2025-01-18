3beaa69fdfa7aec04aabe751f9cdda8382c3d48a
Tim Miller <innerlogic4321@gmail.com>
2025-01-19 01:22:23 +0900

    Adjust Firehose Parsing logic

--- samples/Firehose/Program.cs
+++ samples/Firehose/Program.cs
@@ -54,50 +54,6 @@ async Task HandleMessageAsync(SubscribeRepoMessage message)
     if (message.Record is not null)
     {
         Console.WriteLine($"Record: {message.Record.ToJson()}");
-
-        if (message.Record is Post post)
-        {
-            // The Actor Did.
-            var did = message.Commit.Repo;
-
-            var repo = (await atProtocol.DescribeRepoAsync(did)).HandleResult();
-
-            // Commit.Ops are the actions used when creating the message.
-            // In this case, it's a create record for the post.
-            // The path contains the post action and path, we need the path, so we split to get it.
-            var url = $"https://bsky.app/profile/{did}/post/{message.Commit.Ops![0]!.Path!.Split('/').Last()}";
-            Console.WriteLine($"Post URL: {url}, from {repo.Handle}");
-
-            if (post.Reply is not null)
-            {
-                Console.WriteLine($"Reply Root: {post.Reply.Root}");
-                Console.WriteLine($"Reply Parent: {post.Reply.Parent}");
-            }
-
-            if (post.Embed is EmbedVideo videoEmbed)
-            {
-                // https://video.bsky.app/watch/did%3Aplc%3Acxe5e4ldjfvryf5dqvopdq3v/bafkreiefakrdmclohastskuauwurbtx3tnu2drjpnirsoroyalq5nqr73a/playlist.m3u8
-                var link = videoEmbed.Video?.Ref?.Link;
-                var linkString = link.ToString();
-                Console.WriteLine($"Video Link: https://video.bsky.app/watch/{did}/{linkString}/playlist.m3u8");
-            }
-        }
-
-        static string sha256_hash(string value)
-        {
-            StringBuilder Sb = new StringBuilder();
-
-            using (SHA256 hash = SHA256.Create())
-            {
-                Encoding enc = Encoding.UTF8;
-                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
-
-                foreach (Byte b in result)
-                    Sb.Append(b.ToString("x2"));
-            }
-
-            return Sb.ToString();
-        }
     }
 }
 
