b3b490951c55f391c6ca2a7589918c2dddb04a49
Tim Miller <drasticactions@users.noreply.github.com>
2025-01-18 22:43:46 +0900

    Add missing Firehose Frames (#165)

---
+++ src/FishyFlip/Models/FrameIdentity.cs
@@ -0,0 +1,43 @@
+// <copyright file="FrameIdentity.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace FishyFlip.Models;
+
+/// <summary>
+/// Frame Identity.
+/// </summary>
+public class FrameIdentity
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FrameIdentity"/> class.
+    /// </
+    /// <param name="obj">The CBOR object containing the identity information.</param>
+    public FrameIdentity(CBORObject obj)
+    {
+        this.Seq = obj["seq"].AsInt64Value();
+        this.Did = obj["did"] is not null ? ATDid.Create(obj["did"].AsString()) : null;
+        this.Time = obj["time"]?.ToDateTime();
+        this.Handle = obj["handle"] is not null ? ATHandle.Create(obj["handle"].AsString()) : null;
+    }
+
+    /// <summary>
+    /// Gets the did.
+    /// </summary>
+    public ATDid? Did { get; }
+
+    /// <summary>
+    /// Gets the sequence.
+    /// </summary>
+    public long Seq { get; }
+
+    /// <summary>
+    /// Gets the date time of the frame.
+    /// </summary>
+    public DateTime? Time { get; }
+
+    /// <summary>
+    /// Gets the handle.
+    /// </summary>
+    public ATHandle? Handle { get; }
+}
