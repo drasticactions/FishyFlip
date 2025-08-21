10d727f3f0b95883a0d8cf779dc4e8b311d40075
Timothy Miller <innerlogic4321@gmail.com>
2025-08-21 17:39:20 +0900

    Start MVC sandbox

---
+++ sandbox/SandboxWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+// <copyright file="HomeController.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SandboxWebMvc.Models;
+
+namespace SandboxWebMvc.Controllers;
+
+/// <summary>
+/// Home controller.
+/// </summary>
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HomeController"/> class.
+    /// </summary>
+    /// <param name="logger">Logger.</param>
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Returns the Index view.
+    /// </summary>
+    /// <returns><see cref="IActionResult"/>.</returns>
+    public IActionResult Index()
+    {
+        return this.View();
+    }
+
+    /// <summary>
+    /// Returns the Privacy view.
+    /// </summary>
+    /// <returns><see cref="IActionResult"/>.</returns>
+    public IActionResult Privacy()
+    {
+        return this.View();
+    }
+
+    /// <summary>
+    /// Handles errors.
+    /// </summary>
+    /// <returns><see cref="IActionResult"/>.</returns>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+    }
+}
---
+++ sandbox/SandboxWebMvc/Program.cs
@@ -0,0 +1,33 @@
+// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
