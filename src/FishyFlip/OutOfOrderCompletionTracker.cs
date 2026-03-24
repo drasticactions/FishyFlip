0e812f6741a85d85aa1b05d42a3f7111bc300ef0
alnkesq <141324281+alnkesq@users.noreply.github.com>
2026-03-24 19:19:27 +0100

    LastDefinitelyProcessedCursor for JetStream and ATProto clients

---
+++ src/FishyFlip/OutOfOrderCompletionTracker.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="OutOfOrderCompletionTracker.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+using System.Runtime.CompilerServices;
+
+namespace FishyFlip
+{
+    /// <summary>
+    /// Keeps track of the most recent event ID such that all previous events have definitely been processed.
+    /// </summary>
+    internal class OutOfOrderCompletionTracker
+    {
+        private readonly Dictionary<long, long?> eventIdToSeq = new();
+        private long lastDefinitelyProcessedEventId;
+        private long? lastDefinitelyProcessedSeq;
+        private long lastGeneratedEventId;
+
+        /// <summary>
+        /// Gets the last firehose cursor such that this and all previous events have been already processed on the threadpool.
+        /// </summary>
+        public long? LastDefinitelyProcessedSeq
+        {
+            get
+            {
+                lock (this)
+                {
+                    return this.lastDefinitelyProcessedSeq;
+                }
+            }
+        }
+
+        /// <summary>
+        /// To be called when an event is generated but not processed yet. Each call must eventually result in a <see cref="OnEventProcessed"/>.
+        /// </summary>
+        /// <returns>A sequentially increasing event ID that must be eventually passed to <see cref="OnEventProcessed"/>.</returns>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public long OnEventGenerated()
+        {
+            return ++this.lastGeneratedEventId;
+        }
+
+        /// <summary>
+        /// To be called when an event is fully processed (successfully or unsuccessfully). Each call must come after a corresponding <see cref="OnEventGenerated"/>.
+        /// </summary>
+        /// <param name="eventId">The event ID.</param>
+        /// <param name="seq">The firehose cursor of the current entry, if available.</param>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void OnEventProcessed(long eventId, long? seq)
+        {
+            if (eventId <= this.lastDefinitelyProcessedEventId)
+            {
+                throw new InvalidOperationException("OnEventProcessed was called with an eventId that was already supposedly processed.");
+            }
+
+            this.eventIdToSeq.Add(eventId, seq);
+            this.UpdateLastDefinitelyProcessedEvent();
+        }
+
+        private void UpdateLastDefinitelyProcessedEvent()
+        {
+            while (this.eventIdToSeq.TryGetValue(this.lastDefinitelyProcessedEventId + 1, out var seq))
+            {
+                this.lastDefinitelyProcessedEventId++;
+                this.lastDefinitelyProcessedSeq = seq;
+                this.eventIdToSeq.Remove(this.lastDefinitelyProcessedEventId);
+            }
+        }
+    }
+}
+
+
